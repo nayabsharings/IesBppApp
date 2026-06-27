2b565f1afbb5dd077ab51f902e06c0e84556b48c
nayabsharings <nayab.github@gmail.com>
2026-06-27 12:49:20 +0530

    First commit

---
+++ src/test/java/com/apepdcl/IesBapAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apepdcl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IesBapAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

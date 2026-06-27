d77d3cebde4d93fadf0fb5f30076556449914510
nayabsharings <nayab.github@gmail.com>
2026-06-27 12:54:27 +0530

    BPP files first commit

---
+++ src/main/java/com/apepdcl/IesBppAppApplication.java
@@ -0,0 +1,13 @@
+package com.apepdcl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IesBppAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IesBppAppApplication.class, args);
+	}
+
+}
